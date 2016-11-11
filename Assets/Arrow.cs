6200e5877638c7405ba09f9bf3903d9b0a0b393b
zxcjqrw <oscpe391@student.liu.se>
2016-11-12 00:59:35 +0100

    Added tree with arrows

---
+++ Assets/Arrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+    private Transform body;
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        body = gameObject.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        body.position += body.up*speed*Time.deltaTime;
+	}
+}
---
+++ Assets/CreateArrows.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateArrows : MonoBehaviour {
+
+    public GameObject Arrow;
+    public Transform spawnPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Arrow, spawnPos.position, spawnPos.rotation);
+        }
+	
+	}
+}
