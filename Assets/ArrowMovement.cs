cdd23403142ef173458fca9996b958a3c101b04a
zxcjqrw <oscpe391@student.liu.se>
2016-11-12 15:02:21 +0100

    boomerangs works

---
+++ Assets/ArrowMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMovement : MonoBehaviour
+{
+
+    private Transform body;
+    public float speed;
+    public Transform enemyTransform;
+    public float proximity;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = gameObject.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (enemyTransform.position - body.position);
+        
+        body.position += direction.normalized * speed * Time.deltaTime;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        rotation = Quaternion.Slerp(transform.rotation, rotation, 1);
+        transform.rotation = rotation * transform.rotation;
+        //Vector3.Slerp(body.forward, direction.normalized, 1);
+        if (direction.magnitude < proximity)
+            Destroy(gameObject);
+    }
+}
--- Assets/CreateArrows.cs
+++ Assets/CreateArrows.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class CreateArrows : MonoBehaviour {
 
-    public GameObject Arrow;
-    public Transform spawnPos;
+    public float Range;
+    public float shootDelay;
+    public GameObject Projectile;
+    private float lastTick;
 
 	// Use this for initialization
 	void Start () {
+        lastTick = Time.time;
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Fire1"))
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(Time.time + " " + lastTick + "" + shootDelay);
+        if (Time.time > lastTick + shootDelay)
         {
-            Instantiate(Arrow, spawnPos.position, spawnPos.rotation);
+            for (int i = 0; i < gameObject.transform.parent.childCount; i++)
+            {
+                Transform enemyTransform = gameObject.transform.parent.GetChild(i);
+                if (Vector3.Distance(enemyTransform.position, gameObject.transform.position) < Range)
+                {
+                    Shoot(enemyTransform);
+                }
+            }
         }
-	
-	}
+        lastTick = Time.time;
+    }
+    void Shoot(Transform enemyTransform)
+    {
+        Vector3 direction = (enemyTransform.position - gameObject.transform.position);
+        GameObject arrow = Instantiate(Projectile, gameObject.transform.position, Quaternion.Euler(100, 0, 0)) as GameObject;
+        arrow.GetComponent<ArrowMovement>().enemyTransform = enemyTransform;
+    }
+    //Vector3.Angle(gameObject.transform.position, direction)
 }
