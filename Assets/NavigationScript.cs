d7aca164971a04464a657627783aa4ab7b758ae0
U-Läderlaptopen\Micke <Micke@Lderlaptopen.(none)>
2016-11-12 17:46:19 +0100

    fucking pathfinding and shit bro

--- Assets/NavigationScript.cs
+++ Assets/NavigationScript.cs
@@ -1,39 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using AggregatGames.AI.Pathfinding;
 
 public class NavigationScript : MonoBehaviour {
 
-
+	public Transform target;
 	private NavMeshAgent agent;
-	private List<Transform> destinationPoints;
-	private int currentTarget = 0;
+
+
+	public float speed = 0.5f;
+	public float reachedKnot = 2f;
 
 	// Use this for initialization
 	void Start () {
 		agent = gameObject.GetComponent<NavMeshAgent>();
-		destinationPoints = new List<Transform>();
-
-		Transform parentTransform = gameObject.transform.parent.gameObject.transform;
-		foreach(Transform child in parentTransform)
-		{
-			if (child.name.Contains("DestinationPoint"))
-				destinationPoints.Add(child);
-		}
-
-		agent.SetDestination(destinationPoints[currentTarget].transform.position);
-
+		agent.SetDestination(target.position);
 	}
-	
-	void ArrivedAtTarget()
-	{
-		currentTarget = (currentTarget + 1) % destinationPoints.Count;
-		agent.SetDestination(destinationPoints[currentTarget].transform.position);
-	}
+
 
 	// Update is called once per frame
-	void Update () {
-		if (Vector3.Distance(gameObject.transform.position, agent.destination) < 30)
-			ArrivedAtTarget();
+	void Update() {
+
 	}
 }
--- Assets/TreePlacement.cs
+++ Assets/TreePlacement.cs
@@ -5,11 +5,10 @@ public class TreePlacement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
