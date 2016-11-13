e7b316a624634e0e470fbab59f00e6cdab8bdd1d
U-Läderlaptopen\Micke <Micke@Lderlaptopen.(none)>
2016-11-13 12:39:25 +0100

    added deleted files back in

---
+++ Assets/Woodcutter - Copy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Woodcutter : MonoBehaviour {
+
+    public float Range;
+    public float cutDelay;
+    private float lastTick;
+	public GameObject TreeList;
+	public PlayerScript player;
+
+	// Use this for initialization
+	void Start () {
+        lastTick = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > lastTick + cutDelay)
+        {
+            for (int i = 0; i < TreeList.transform.childCount; i++)
+            {
+				//Transform treeTransform = gameObject.transform.parent.GetChild(i);
+				Transform treeTransform = TreeList.transform.GetChild(i).transform;
+                if (Vector3.Distance(treeTransform.position, gameObject.transform.position) < Range)
+                {
+                    Cutting(treeTransform);
+					break;
+                }
+            }
+            lastTick = Time.time;
+        }
+    }
+    void Cutting(Transform treeTransform)
+    {
+		int lumberReward = treeTransform.gameObject.GetComponent<ClickableTreeScript>().CutDownTree();
+		player.CurrentLumber += lumberReward;
+	}
+}
